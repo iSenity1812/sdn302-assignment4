7d469eeb8d122cd7df5d01bd8d056ee2d53a8fa7
iSenity1812 <pvduchuya4@gmail.com>
2026-03-09 03:20:10 +0700

    fix: env

--- apps/server/src/infrastructure/database/mongo.client.ts
+++ apps/server/src/infrastructure/database/mongo.client.ts
@@ -1,16 +1,23 @@
 import { envConfig } from "@/config/env.config";
 import { MongoClient } from "mongodb";
 import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 export async function createMongoClient(): Promise<MongoClient> {
   await mongoose.connect(envConfig.mongodbUri, {
-    dbName: envConfig.mongodbDbName,
+    dbName: process.env.MONGODB_DB_NAME || "QuizApp",
   });
 
-  const client = new MongoClient(envConfig.mongodbUri);
+  const client = new MongoClient(
+    process.env.MONGODB_URI || "mongodb://localhost:27017",
+  );
   await client.connect();
   // Optionally, you can verify the connection by pinging the database
-  await client.db(envConfig.mongodbDbName).command({ ping: 1 });
+  await client
+    .db(process.env.MONGODB_DB_NAME || "QuizApp")
+    .command({ ping: 1 });
   console.log("Connected to MongoDB successfully");
   return client;
 }
--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,11 +1,14 @@
 import "reflect-metadata";
 import { createApp } from "./app";
 import { envConfig } from "./config/env.config";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await createApp();
-  const port = envConfig.port;
-  const host = envConfig.host;
+  const port = Number(process.env.PORT) || 3002;
+  const host = process.env.HOST || "0.0.0.0";
   app.listen(port, host, () => {
     console.log(`Server is running at http://${host}:${port}`);
   });
