476ee0c237463cc40e4d29f7937a055e622eeef8
iSenity1812 <pvduchuya4@gmail.com>
2026-03-09 07:58:12 +0700

    test: logging db info

--- apps/server/src/infrastructure/database/mongo.client.ts
+++ apps/server/src/infrastructure/database/mongo.client.ts
@@ -6,13 +6,14 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export async function createMongoClient(): Promise<MongoClient> {
+  console.log("Connecting to MongoDB...");
+  console.log("MONGODB_URI:", process.env.MONGODB_URI);
+  console.log("MONGODB_DB_NAME:", process.env.MONGODB_DB_NAME);
   await mongoose.connect(envConfig.mongodbUri, {
-    dbName: process.env.MONGODB_DB_NAME || "QuizApp",
+    dbName: process.env.MONGODB_DB_NAME as string,
   });
 
-  const client = new MongoClient(
-    process.env.MONGODB_URI || "mongodb://localhost:27017",
-  );
+  const client = new MongoClient(process.env.MONGODB_URI as string);
   await client.connect();
   // Optionally, you can verify the connection by pinging the database
   await client
